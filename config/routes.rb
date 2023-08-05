a557cc2f8b6e86c3525d8e57bdeb8d66ef6bc61c
akshay <chavanakshaykumar10@gmail.com>
2023-08-06 03:15:03 +0530

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
  
   get 'students/show'
   resources :students
-  # root "students#index"
+  root "students#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
